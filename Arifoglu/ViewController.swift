ad57839d5b9f21138361b942ea2b4ad19d6f9cae
Emirhan Erdogan <emir.hotelmydora@gmail.com>
2015-10-11 14:42:15 +0300

    Initial Commit

---
+++ Arifoglu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Arifoglu
+//
+//  Created by Emirhan Erdogan on 11/10/15.
+//  Copyright © 2015 Tutam Gida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
