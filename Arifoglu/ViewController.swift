4c8ca67fc24239cd0aef080f1e515277d74ee1a4
Emirhan Erdogan <emir.hotelmydora@gmail.com>
2015-10-11 14:43:38 +0300

    regard

--- Arifoglu/ViewController.swift
+++ Arifoglu/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
     
     
-    //Bence çalışır artık.
+    //Bence çalışır artık.4
   }
 
   override func didReceiveMemoryWarning() {
